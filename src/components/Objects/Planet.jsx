8bd44eb77817c3800d97ce2786c74556d8d684db
nitramXmas <martin.noel.fr@gmail.com>
2023-09-15 12:02:47 +0200

    remove gsap animation and set cam lookat sun component

--- src/components/Objects/Planet.jsx
+++ src/components/Objects/Planet.jsx
@@ -1,133 +1,88 @@
-import { Clone, Torus, useGLTF, Center } from '@react-three/drei'
-import { useFrame, useThree } from '@react-three/fiber';
-import { useRef, useState } from 'react';
-import Moon from './Moon';
-import {gsap} from 'gsap';
-import './Planet.css'
-import { Vector3 } from 'three';
+import { Clone, Torus, useGLTF, Center } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import Moon from "./Moon";
+import { gsap } from "gsap";
+import "./Planet.css";
+import { Vector3 } from "three";
 
-const Planet = ({planet, moons, indexObject, indexAstre}) => {
+const Planet = ({ planet, moons, indexObject, indexAstre }) => {
+  let { meanRadius, aphelion, sideralRotation, axialTilt, sideralOrbit } =
+    planet; //let because we modify some value for scale
 
-    let { meanRadius, aphelion, sideralRotation, axialTilt, sideralOrbit } = planet //let because we modify some value for scale
-    
-    sideralRotation /= 100000; //in hours in API => to convert
-    meanRadius /= 10000000;
-    aphelion /= 10000000;
+  sideralRotation /= 100000; //in hours in API => to convert
+  meanRadius /= 10000000;
+  aphelion /= 10000000;
 
-    const planetModel = useGLTF(planet.model3d);
-    const earthOrbit = 365.256;
-    const planetRef = useRef()
-    const turnArroundSun = useRef()
-    const torusRef = useRef()
-    const { camera } = useThree()
+  const planetModel = useGLTF(planet.model3d);
+  const earthOrbit = 365.256;
+  const planetRef = useRef();
+  const turnArroundSun = useRef();
+  const torusRef = useRef();
+  const { camera } = useThree();
 
-    const [isFocus, setIsFocus] = useState(false)
-    
-    let color = 'white';
-    let position = 0
-    let planetPosition = new Vector3();
-    let pos = {}
+  const [isFocus, setIsFocus] = useState(false);
 
-    //conversion des angles de degres (API) vers radians (ThreeJS)
-    const radianAxialTilt = (axialTilt * Math.PI) / 180
+  let color = "white";
+  let position = 0;
+  let planetPosition = new Vector3();
+  let pos = {};
 
-   { indexAstre === indexObject ? color = 'white' : color = 'dimgray'}
+  //conversion des angles de degres (API) vers radians (ThreeJS)
+  const radianAxialTilt = (axialTilt * Math.PI) / 180;
 
-   const gsapEase = 'back(0.7, 0.7, false)'
-   const gsapDuration = 2.5
+  {
+    indexAstre === indexObject ? (color = "white") : (color = "dimgray");
+  }
 
-   useFrame((state)=>{
+  useFrame((state) => {
+    position = planetRef.current.children[0].geometry.getAttribute("position");
+    planetPosition.fromBufferAttribute(position);
+    pos = planetRef.current.children[0].getWorldPosition(planetPosition);
 
-       
-       position = (planetRef.current.children[0].geometry.getAttribute( 'position' ));
-       planetPosition.fromBufferAttribute( position );
-       pos = planetRef.current.children[0].getWorldPosition(planetPosition)
-       
-       const targetPlanet = () => {
+    const targetPlanet = () => {
+      if (!isFocus) {
+        state.camera.lookAt(pos.x - 0.5, 0, pos.z);
+      }
+    };
 
-            state.camera.lookAt(pos.x -0.5 , 0, pos.z )
+    planetRef.current.rotation.y += sideralRotation; //rotation sur elle-meme
+    turnArroundSun.current.rotation.y += earthOrbit / sideralOrbit / 1000; //rotation de la planete autour du soleil
 
-       }
+    targetPlanet();
+  });
 
-        planetRef.current.rotation.y += sideralRotation; //rotation sur elle-meme
-        turnArroundSun.current.rotation.y += (earthOrbit / sideralOrbit) / 1000; //rotation de la planete autour du soleil
-        
-        if (indexAstre === indexObject){
-            
-           if(!isFocus){
-
-                if(meanRadius > 0.0050 )
-
-            { 
-                gsap.to(camera.position, {
-                    x:  pos.x -15,
-                    y:  0,
-                    z:  pos.z +20,
-                    duration: gsapDuration,
-                    ease: gsapEase
-                    })
-                }
-                
-             else if (meanRadius < 0.0050 && meanRadius > 0.0024  )  {
-
-                gsap.to(camera.position, {
-                 x:  pos.x -5,
-                 y:  0,
-                 z: pos.z +8,
-                 duration: gsapDuration,
-                ease: gsapEase
-                })
-
-            } else {
-
-                 gsap.to(camera.position, {
-                 x:  pos.x -1,
-                 y:  0,
-                 z:  pos.z + 2,
-                 duration: gsapDuration,
-                    ease: gsapEase
-                })
-
-            }
-        }
-        targetPlanet()
-        }
-    })
-
-    return (
+  return (
     <>
-        <mesh ref={turnArroundSun}>
+      <mesh ref={turnArroundSun}>
+        <Clone
+          ref={planetRef}
+          object={planetModel.scene}
+          scale={meanRadius}
+          position={[aphelion, 0, 0]}
+          rotation={[0, 0, -radianAxialTilt]}
+          castShadow
+        ></Clone>
 
-            <Clone
-                ref={planetRef}
-                object={ planetModel.scene }
-                scale={ meanRadius }
-                position={ [aphelion, 0 ,0] }
-                rotation={[0, 0 ,-radianAxialTilt]}
-                castShadow
+        <Torus
+          ref={torusRef}
+          args={[aphelion, 0.02, 30, 200, Math.PI * 2 - 1]}
+          rotation={[-Math.PI / 2, 0, Math.PI / 2 - Math.PI / 2.95]}
+          material-color={color}
+        />
+
+        {moons &&
+          moons.map((moon) => (
+            <Center
+              key={moon.id}
+              position={[aphelion + moon.aphelion / 200000, 0, 0]}
             >
-            </Clone>
+              <Moon moon={moon} />
+            </Center>
+          ))}
+      </mesh>
+    </>
+  );
+};
 
-                <Torus
-                    ref={torusRef}
-                    args={[aphelion,0.02,30,200, (Math.PI * 2 )-1]}
-                    rotation={[- Math.PI / 2, 0, (Math.PI / 2)-Math.PI / 2.95]}
-                    material-color = {color}
-                />
-                
-
-                {moons &&
-                moons
-                .map((moon)=>(
-                    <Center key={moon.id}  position={[aphelion + moon.aphelion / 200000 , 0 ,0]}>
-                        <Moon moon = {moon} />
-                    </Center>
-                    ))
-                } 
-        </mesh>
-
-        </>
-    )
-}
-
-export default Planet
+export default Planet;
--- src/pages/Scene.jsx
+++ src/pages/Scene.jsx
@@ -1,97 +1,111 @@
-import { useState, useEffect } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Stars, OrbitControls, PerspectiveCamera } from '@react-three/drei';
-import { SpinnerDotted } from 'spinners-react';
-import axios from 'axios';
-import Header from '../components/Header/Header';
-import Card from '../components/Card/Card';
-import Navbar from '../components/Navbar/Navbar';
-import Sun from '../components/Objects/Sun';
-import Planet from '../components/Objects/Planet';
-import './Scene.css';
+import { useState, useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Stars,
+  OrbitControls,
+  PerspectiveCamera,
+  CameraControls,
+} from "@react-three/drei";
+import { SpinnerDotted } from "spinners-react";
+import axios from "axios";
+import Header from "../components/Header/Header";
+import Card from "../components/Card/Card";
+import Navbar from "../components/Navbar/Navbar";
+import Sun from "../components/Objects/Sun";
+import Planet from "../components/Objects/Planet";
+import "./Scene.css";
 
 const Scene = () => {
+  const [objects, setObjects] = useState([]);
+  const [clicked, setClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [moons, setMoons] = useState({});
 
-    const [objects, setObjects] = useState([]);
-    const [clicked,setClicked] = useState (false)
-    const [isLoading, setIsLoading] = useState(true);
-    const [moons, setMoons] = useState({});
+  const [indexObject, setIndexObject] = useState("");
 
-    const [indexObject, setIndexObject] = useState('')
-    console.log(indexObject);
+  const handleSetObject = (indexObject) => {
+    setIndexObject(indexObject);
+  };
 
-    const handleSetObject = (indexObject) => {
-        setIndexObject( indexObject )
-    }
+  const handleClicked = () => {
+    setClicked(!clicked);
+  };
+  const handleClose = () => {
+    setClicked(false);
+  };
 
-    const handleClicked = ()=>{
-      setClicked(!clicked)
-    }
-    const handleClose = ()=>{
-      setClicked( false )
-    }
+  useEffect(() => {
+    axios
+      .get("https://apollo-api.martinnoel.fr/solar-system/solar-system")
+      .then((res) => {
+        const moonsMap = {};
 
-    useEffect(() => {
-        axios
-          .get('https://apollo-api.martinnoel.fr/solar-system/solar-system')
-          .then((res) => {
+        res.data.bodies.forEach((element) => {
+          if (element.bodyType !== "Moon") {
+            return;
+          }
 
-            const moonsMap = {}
-              
-              res.data.bodies.forEach(element => {
+          if (!moonsMap[element.aroundPlanet.planet]) {
+            moonsMap[element.aroundPlanet.planet] = [];
+          }
 
-                if (element.bodyType !== 'Moon' ) {
-                    return
-                }
+          moonsMap[element.aroundPlanet.planet].push(element);
+        });
+        setObjects(res.data.bodies);
+        setMoons(moonsMap);
+        setIsLoading(false);
+      });
+  }, []);
 
-               if(!moonsMap[element.aroundPlanet.planet]){
-                moonsMap[element.aroundPlanet.planet]=[]
-               }
-
-               moonsMap[element.aroundPlanet.planet].push(element)
-              });
-            setObjects(res.data.bodies);
-            setMoons(moonsMap);
-            setIsLoading(false);
-          });
-      }, []);
-
-    return (
-        <div className='scene'>
-          <Header />
-        <div className="canvas">
-          {isLoading ? (
-            <div className='loading'>
-            <img className='capybara' src="https://i.redd.it/capy-is-king-v0-ian6ahoqhqna1.png?s=5639c3866298be9ecffdfe3c8f34f9f8371e885e" alt="capybara" />
-            <span>Loading</span> 
+  return (
+    <div className="scene">
+      <Header />
+      <div className="canvas">
+        {isLoading ? (
+          <div className="loading">
+            <img
+              className="capybara"
+              src="https://i.redd.it/capy-is-king-v0-ian6ahoqhqna1.png?s=5639c3866298be9ecffdfe3c8f34f9f8371e885e"
+              alt="capybara"
+            />
+            <span>Loading</span>
             <SpinnerDotted color="#424463" />
-            </div>
-            ) : (
-              <>
-              {clicked  ? (
-                <div className="card-list">
-          {objects &&
-           objects
-           .filter((object,index) => object.bodyType === 'Planet' && index === indexObject || object.bodyType === 'Star' && index === indexObject || object.bodyType === 'Moon' && index === indexObject )
-           .map((planet) => {
-             return (
-           <Card key={planet.id} 
-                 scenePlanet={planet}
-                 handleClose={handleClose}
-           />)
-           })}
-      </div>) : null }
-            <Canvas
-            shadows
+          </div>
+        ) : (
+          <>
+            {clicked ? (
+              <div className="card-list">
+                {objects &&
+                  objects
+                    .filter(
+                      (object, index) =>
+                        (object.bodyType === "Planet" &&
+                          index === indexObject) ||
+                        (object.bodyType === "Star" && index === indexObject) ||
+                        (object.bodyType === "Moon" && index === indexObject)
+                    )
+                    .map((planet) => {
+                      return (
+                        <Card
+                          key={planet.id}
+                          scenePlanet={planet}
+                          handleClose={handleClose}
+                        />
+                      );
+                    })}
+              </div>
+            ) : null}
+            <Canvas shadows>
+              <PerspectiveCamera
+                makeDefault
+                position={[-70, 70, 70]}
+                fov={45}
+                near={0.1}
+                far={6000}
+                lookAt={<Sun />}
+              />
 
-            >
-                <PerspectiveCamera makeDefault 
-                                    position={[-70, 70, 70]}
-                                    fov={45}
-                                    near={0.1}
-                                    far={6000} />
-
-                <OrbitControls />
+              <CameraControls makeDefault />
 
               <Stars
                 radius={500}
@@ -103,29 +117,30 @@ const Scene = () => {
                 speed={0}
               />
 
-              <pointLight 
-                intensity={0.5}
-                castShadow
-                />
+              <pointLight intensity={0.5} castShadow />
 
               {objects &&
-                objects
-                  .map((astre, indexAstre) => {
-                     if (astre.bodyType === 'Star') return <Sun key={astre.id} sun={astre}  />                                             
-                     if (astre.bodyType === 'Planet') return <Planet key={astre.id} 
-                                                                     planet={astre} 
-                                                                     moons={moons[astre.id]} 
-                                                                     indexObject={indexObject} 
-                                                                     indexAstre={indexAstre} />
-                    return null;
-                    })}
+                objects.map((astre, indexAstre) => {
+                  if (astre.bodyType === "Star")
+                    return <Sun key={astre.id} sun={astre} />;
+                  if (astre.bodyType === "Planet")
+                    return (
+                      <Planet
+                        key={astre.id}
+                        planet={astre}
+                        moons={moons[astre.id]}
+                        indexObject={indexObject}
+                        indexAstre={indexAstre}
+                      />
+                    );
+                  return null;
+                })}
             </Canvas>
-            </>
-              )}
-        </div>
-        <Navbar handleSetObject={handleSetObject}
-                handleClicked={handleClicked}/>
+          </>
+        )}
       </div>
-    );
-  };
-  export default Scene;
+      <Navbar handleSetObject={handleSetObject} handleClicked={handleClicked} />
+    </div>
+  );
+};
+export default Scene;
